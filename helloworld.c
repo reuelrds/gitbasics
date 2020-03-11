4cf9ddeab772a1d93ddb173a888a6b5fa03ee2a3
7998 <7998@acer7.fragnel.ac.in>
2020-03-11 12:59:17 +0530

    testing for merge conflicts

--- helloworld.c
+++ helloworld.c
@@ -4,5 +4,6 @@ int main() {
 	printf("Hello World! I'm learning git basics");
 	printf("Edited through cloned repository");
 	printf("Edited on a new branch");
+	printf("Testing for merge conflicts");
 	return 0;
 }
